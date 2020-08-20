7d671a1356026b1e89e477722ad4a97f85cbf613
Никита <killerlord1988@gmail.com>
2020-08-20 19:27:10 +0400

    add localStorage

--- js/script.js
+++ js/script.js
@@ -11,6 +11,19 @@
   const counterAllTask = counter.querySelector('.counter-all span');
   const counterCompletedTask = counter.querySelector('.counter-completed span');
   const counterWaitTask = counter.querySelector('.counter-wait span');
+  
+  function renderTask() {
+    const raw = localStorage.getItem('tasks')
+    const tasksArr = JSON.parse(raw)
+    tasksArr.forEach(el =>{
+      const task = newItemTemplate.cloneNode(true);
+      const taskDescription = task.querySelector('span');
+      taskDescription.textContent = el;
+      list.appendChild(task);
+    })
+  }
+  renderTask()
+  
   let checkboxs = document.querySelectorAll('.todo-list-input');
 
   function counterUpdate() {
@@ -18,12 +31,26 @@
     let completedTask = counterCompletedTask.textContent = toggleClassCompleted()
     counterWaitTask.textContent = totalTask - completedTask
   }
-
   counterUpdate()
 
   function updateCheckboxsCollection() {
     checkboxs = document.querySelectorAll('.todo-list-input');
+    writeLocalStorage()
+    localStorage.setItem('tasks', JSON.stringify(writeLocalStorage()))
   }
+
+  function writeLocalStorage() {
+    const textTaskList = document.querySelectorAll('.todo-list-item span')
+    let taskCollection = []
+    Object.keys(textTaskList).forEach(function (el) {
+      if (textTaskList[el].className !== 'visually-hidden') {
+        taskCollection.push(textTaskList[el].textContent)
+      }
+    });
+    return taskCollection
+  }
+
+
 
   function toggleEmptyMessage() {
     if (items.length === 0) {
@@ -31,7 +58,8 @@
     } else {
       emptyListMessage.classList.add('hidden');
     }
-  };
+  }
+  toggleEmptyMessage()
 
   function removingTaskOnButton(item) {
     const removeButton = item.querySelector('.todo-list-remove');
@@ -46,24 +74,24 @@
   Object.keys(items).forEach(function (el) {
     removingTaskOnButton(items[el]);
   });
-  
-  
+
+
   function toggleClassCompleted() {
     let countCompleted = 0
     Object.keys(checkboxs).forEach(function (el) {
-        if (checkboxs[el].checked) {
-          items[el].classList.add('completed')
-          countCompleted++
-        } else {
-          items[el].classList.remove('completed')
-        }
-      });
-      return countCompleted
-    };
+      if (checkboxs[el].checked) {
+        items[el].classList.add('completed')
+        countCompleted++
+      } else {
+        items[el].classList.remove('completed')
+      }
+    });
+    return countCompleted
+  };
 
   toggleClassCompleted()
 
-  list.addEventListener('change', function(evt) {
+  list.addEventListener('change', function (evt) {
     if (evt.target.className != items) toggleClassCompleted();
     counterUpdate()
   });
